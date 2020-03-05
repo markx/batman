804fc5a80dbe96478442c1d804b2df6603e255e7
Ke Xu <kdotxu@gmail.com>
2020-03-04 23:05:27 -0800

    add preset script namespace

--- src/batman/script.clj
+++ src/batman/script.clj
@@ -1,5 +1,20 @@
-(ns batman.script)
-             
+(ns batman.script
+  (:require [clojure.string :as string])
+  (:require [batman.core :refer [register-alias! register-trigger! handle-code]]))
 
 
-(comment)
+(defn- transform [p line]
+  (let [[f xs] (next (re-find p line))]
+    (if (empty? xs)
+      (format "(%s)" f)
+      (format "(%s \"%s\")" f (apply str xs)))))
+
+(def command-pattern #"/([\w-/]+)((?:\s+\w+)*)\s*$")
+(register-alias! command-pattern
+                  (fn [line]
+                    (batman.core/handle-code (transform command-pattern line)))
+                  9999)
+
+(comment
+  (re-find command-pattern "/heal/pattern")
+  (string/replace "/heal/toggle" command-pattern "($1 \"$2\")"))
